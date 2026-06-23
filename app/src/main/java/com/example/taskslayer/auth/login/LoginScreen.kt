466b6a2971c72f2f3b9b8fc3ee11107b17266e8f
Gianluca <gianlucamantovaneli@gmail.com>
2026-06-23 17:02:10 -0300

    Ajustando dependencias

---
+++ app/src/main/java/com/example/taskslayer/auth/login/LoginScreen.kt
@@ -0,0 +1,15 @@
+package com.example.taskslayer.auth.login
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class LoginScreen {
+
+    @Composable
+    fun Login(
+
+    ){
+        Text(text = "Login")
+    }
+
+}
---
+++ app/src/main/java/com/example/taskslayer/auth/login/LoginViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.taskslayer.auth.login
+
+class LoginViewModel {
+}
