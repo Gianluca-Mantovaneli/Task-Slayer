efb3b01bd39d30309e9e39142afb58cf340ac21b
Gianluca <gianlucamantovaneli@gmail.com>
2026-07-03 17:32:20 -0300

    feat: Iniciando as telas de adicionar tasks Fix: Botao flutuante não passa mais por cima dos menus, e continua arrastavel

--- app/src/main/java/com/example/taskslayer/home/AddTodoTaskScreen.kt
+++ app/src/main/java/com/example/taskslayer/home/AddTodoTaskScreen.kt
@@ -1,2 +1,26 @@
 package com.example.taskslayer.home
 
+
+import android.content.res.Configuration
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.taskslayer.ui.theme.TaskSlayerTheme
+
+
+@Composable
+fun AddTodoTaskRoute(){
+    AddTodoTaskContent()
+}
+
+@Composable
+fun AddTodoTaskContent(){
+
+}
+
+@Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun AddTodoTaskContentPreview(){
+    TaskSlayerTheme {
+        AddDailieTaskContent()
+    }
+}
